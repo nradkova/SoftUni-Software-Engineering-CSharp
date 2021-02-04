f08417fe8be2065f331f2cf4a7eec4cdbb03f2e0
nradkova <neliradkovaa@gmail.com>
2021-02-05 01:24:33 +0200

    Assignments

---
+++ C#OOP/OOPEncapsulationLab/03.Validation/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                Person person = null;
+                try
+                {
+                person = new Person(cmdArgs[0],
+                                        cmdArgs[1],
+                                        int.Parse(cmdArgs[2]),
+                                        decimal.Parse(cmdArgs[3]));
+
+                }
+                catch (ArgumentException e)
+                {
+
+                    Console.WriteLine(e.Message);
+                }
+                if (person!=null)
+                {
+                persons.Add(person);
+                }
+            }
+            var percentage = decimal.Parse(Console.ReadLine());
+            persons.ForEach(p => p.IncreaseSalary(percentage));
+            persons.ForEach(p => Console.WriteLine(p.ToString()));
+
+        }
+    }
+}
---
+++ C#OOP/OOPEncapsulationLab/04.Team/Team.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            Name = name;
+            firstTeam = new List<Person>();
+            reserveTeam = new List<Person>();
+        }
+        public string Name { get; set; }
+        public ReadOnlyCollection<Person> FirstTeam
+        {
+            get { return firstTeam.AsReadOnly(); }
+        }
+        public ReadOnlyCollection<Person> ReserveTeam
+        {
+            get { return reserveTeam.AsReadOnly(); }
+        }
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                this.firstTeam.Add(person);
+            }
+            else
+            {
+                this.reserveTeam.Add(person);
+            }
+
+        }
+    }
+}
