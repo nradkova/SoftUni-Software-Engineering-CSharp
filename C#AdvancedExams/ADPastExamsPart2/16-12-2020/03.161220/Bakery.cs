bbdf3311b7b26138f8fac46b12c423a2a8291fd7
nradkova <neliradkovaa@gmail.com>
2021-02-17 21:00:59 +0200

    ExamPreparation

---
+++ C#AdvancedExams/ADPastExamsPart2/16-12-2020/03.161220/Bakery.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BakeryOpenning
+{
+   public  class Bakery
+    {
+        private List<Employee> data;
+        public Bakery(string name,int capacity)
+        {
+            data = new List<Employee>();
+            Name = name;
+            Capacity = capacity;
+        }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Count => data.Count;
+        public void Add(Employee employee)
+        {
+            if (data.Count<Capacity)
+            {
+                data.Add(employee);
+            }
+        }
+        public bool Remove(string name)
+        {
+            var employee = data.FirstOrDefault(x => x.Name == name);
+            return data.Remove(employee);
+        }
+        public Employee GetOldestEmployee()
+        {
+            return data.OrderByDescending(x => x.Age).First();
+        }
+        public Employee GetEmployee(string name)
+        {
+            return data.FirstOrDefault(x => x.Name == name);
+        }
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Employees working at Bakery {Name}:");
+            foreach (var employee in data)
+            {
+                sb.AppendLine(employee.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
