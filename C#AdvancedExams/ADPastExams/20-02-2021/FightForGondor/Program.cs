9c788add23c06bed0b4c6f5d9f0ca204568e4171
nradkova <neliradkovaa@gmail.com>
2021-04-13 10:49:17 +0300

    ExamPreparation

---
+++ C#AdvancedExams/ADPastExams/20-02-2021/FightForGondor/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _200221
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int waves = int.Parse(Console.ReadLine());
+
+            Queue<int> plates
+                = new Queue<int>(Console.ReadLine()
+                .Split().Select(int.Parse));
+            Stack<int> orcs = null;
+            bool peopleWin = false;
+            bool orcsWin = false;
+            for (int i = 1; i <= waves; i++)
+            {
+                orcs = new Stack<int>(Console.ReadLine()
+                       .Split().Select(int.Parse));
+
+                if (i % 3 == 0)
+                {
+                    plates.Enqueue(int.Parse(Console.ReadLine()));
+                }
+
+                var plate = plates.Peek();
+                while (orcs.Count > 0 && plates.Count > 0)
+                {
+                    var orc = orcs.Pop();
+                    if (orc == plate)
+                    {
+                        plates.Dequeue();
+                    }
+                    else if (orc > plate)
+                    {
+                        orc -= plate;
+                        orcs.Push(orc);
+                        plates.Dequeue();
+                        if (plates.Count > 0)
+                        {
+                            plate = plates.Peek();
+                        }
+                    }
+                    else if (plate > orc)
+                    {
+                        plate -= orc;
+                        if (orcs.Count > 0)
+                        {
+                            orc = orcs.Peek();
+                        }
+                    }
+                }
+                if (plates.Count == 0)
+                {
+                    orcsWin = true;
+                    break;
+                }
+                if (i == waves && orcs.Count == 0)
+                {
+                    peopleWin = true;
+                }
+            }
+
+
+            if (orcsWin)
+            {
+                Console.WriteLine("The orcs successfully" +
+                    " destroyed the Gondor's defense.");
+                Console.WriteLine($"Orcs left: {string.Join(", ", orcs)}");
+            }
+            if (peopleWin)
+            {
+                Console.WriteLine("The people successfully" +
+                    " repulsed the orc's attack.");
+                Console.WriteLine($"Plates  left: {string.Join(", ", plates)}");
+            }
+        }
+    }
+}
---
+++ C#AdvancedExams/ADPastExams/20-02-2021/Warships/Program.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Linq;
+
+namespace Warships
+{
+    class Program
+    {
+        private static int playerShips = 0;
+        private static int enemyShips = 0;
+        private static string[,] table;
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] command = Console.ReadLine()
+                .Split(",");
+            table = ReadTable(n);
+
+            int playerInitialShips = playerShips;
+            int enemyInitialShips = enemyShips;
+            bool playerWin = false;
+            bool enemyWin = false;
+
+            for (int i = 1; i <= command.Length; i++)
+            {
+                int[] commandArgs = command[i - 1]
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                int currentRow = commandArgs[0];
+                int currentCol = commandArgs[1];
+                if (!AreValid(currentRow, currentCol))
+                {
+                    continue;
+                }
+
+                if (i % 2 != 0)
+                {
+                    PlayerAttacks(currentRow,currentCol);
+                    if (enemyShips==0)
+                    {
+                        playerWin = true;
+                        break;
+                    }
+                }
+                else
+                {
+                    EnemyAttacks(currentRow, currentCol);
+                    if (playerShips == 0)
+                    {
+                        enemyWin = true;
+                        break;
+                    }
+                }
+
+                if (playerWin)
+                {
+                    break;
+                }
+                if (enemyWin)
+                {
+                    break;
+                }
+            }
+
+            int allSunkShips = playerInitialShips
+                + enemyInitialShips
+                - playerShips
+                - enemyShips;
+            if (playerWin)
+            {
+                Console.WriteLine($"Player One has won the game!" +
+                    $" {allSunkShips} " +
+                    $"ships have been sunk in the battle.");
+            }
+            else if (enemyWin)
+            {
+                Console.WriteLine($"Player Two has won the game!" +
+                    $" {allSunkShips} " +
+                    $"ships have been sunk in the battle.");
+            }
+            else
+            {
+                Console.WriteLine($"It's a draw! " +
+                    $"Player One has {playerShips} ships left." +
+                    $" Player Two has {enemyShips} ships left.");
+            }
+        }
+
+        private static void EnemyAttacks(int row, int col)
+        {
+            if (table[row, col] == "<")
+            {
+                table[row, col] = "X";
+                playerShips--;
+            }
+            if (table[row, col] == "#")
+            {
+                DestroyMine(row, col);
+            }
+        }
+
+        private static void PlayerAttacks(int row, int col)
+        {
+            if (table[row,col]==">")
+            {
+                table[row, col] = "X";
+                enemyShips--;
+            }
+            if (table[row,col]=="#")
+            {
+                DestroyMine(row, col);
+            }
+        }
+
+        private static void DestroyMine(int row, int col)
+        {
+            if (AreValid(row-1,col-1))
+            {
+                DestroyShips(row - 1, col - 1);
+            }
+
+            if (AreValid(row - 1, col))
+            {
+                DestroyShips(row - 1, col);
+            }
+
+            if (AreValid(row - 1, col + 1))
+            {
+                DestroyShips(row - 1, col + 1);
+            }
+
+            if (AreValid(row , col - 1))
+            {
+                DestroyShips(row , col - 1);
+            }
+
+            if (AreValid(row , col +1))
+            {
+                DestroyShips(row , col+1);
+            }
+
+            if (AreValid(row +1, col - 1))
+            {
+                DestroyShips(row + 1, col - 1);
+            }
+
+            if (AreValid(row +1, col))
+            {
+                DestroyShips(row + 1, col);
+            }
+
+            if (AreValid(row + 1, col + 1))
+            {
+                DestroyShips(row + 1, col + 1);
+            }
+        }
+
+        private static void DestroyShips(int row, int col)
+        {
+            if (table[row,col]=="<")
+            {
+                table[row, col] = "X";
+                playerShips--;
+            }
+            if (table[row, col] == ">")
+            {
+                table[row, col] = "X";
+                enemyShips--;
+            }
+        }
+
+        private static bool AreValid(int row, int col)
+        {
+            if (row < 0 || row >= table.GetLength(0))
+            {
+                return false;
+            }
+            if (col < 0 || col >= table.GetLength(1))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private static string[,] ReadTable(int n)
+        {
+            string[,] result = new string[n, n];
+            for (int row = 0; row < n; row++)
+            {
+                string[] lineArgs = Console.ReadLine()
+                    .Split();
+                for (int col = 0; col < n; col++)
+                {
+                    result[row, col] = lineArgs[col];
+                    if (result[row, col] == "<")
+                    {
+                        playerShips++;
+                    }
+                    if (result[row, col] == ">")
+                    {
+                        enemyShips++;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
